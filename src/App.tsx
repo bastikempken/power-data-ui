13d8d5d174f0f84ff448f970d7d45097701d06cf
Sebastian Kempken <sebastian@kempken.io>
2025-01-26 14:36:05 +0100

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  IxApplication,
+  IxApplicationHeader,
+  IxContent,
+  IxContentHeader,
+  IxMenu,
+  IxMenuItem,
+} from '@siemens/ix-react';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <IxApplication class='w-screen h-screen'>
+      <IxApplicationHeader name="My Application">
+        <div className="placeholder-logo" slot="logo"></div>
+      </IxApplicationHeader>
+      <IxMenu>
+        <IxMenuItem>Item 1</IxMenuItem>
+        <IxMenuItem>Item 2</IxMenuItem>
+      </IxMenu>
 
-export default App
+      <IxContent>
+        <IxContentHeader
+          slot="header"
+          header-title="My Content Page"
+        ></IxContentHeader>
+      </IxContent>
+    </IxApplication>
+  );
+};
